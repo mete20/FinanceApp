0abb2c41a51a36a1ea4c4f9e0dbb2eaa92ce45e7
Glnisa <91845548+Glnisa@users.noreply.github.com>
2024-01-07 20:22:32 +0300

    crud_portfolio.py is created

---
+++ server/app/crud/crud_portfolio.py
@@ -0,0 +1,13 @@
+from sqlalchemy.orm import Session
+
+
+"""
+def get_portfolio(db: Session, user_id: int):
+    return db.query(model_portfolio.Portfolio)\
+             .filter(model_portfolio.Portfolio.user_id == user_id)\
+             .all()  
+             
+             
+       
+             
+"""
